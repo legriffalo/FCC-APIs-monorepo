c07f2ea51ec803d9b3241aa073d38b18b48076ec
Nick-legriffalo-Grif <56260217+legriffalo@users.noreply.github.com>
2025-04-19 15:35:29 +0300

    added som error catches
    quite clumsy but works so far
    could trim down but will do for such a small porject

--- API1-time/server.js
+++ API1-time/server.js
@@ -18,19 +18,49 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
-});
+// // your first API endpoint...
+// app.get("/api/hello", function (req, res) {
+//   res.json({ greeting: "hello API" });
+// });
 
-app.get("/api/:s/", function (req, res) {
-  const date = new Date(req.params.s);
+app.get("/api", function (req, res) {
+  const date = new Date(Date.now());
   const unix = date.getTime();
   const utc = date.toUTCString();
   res.json({
     unix: unix,
     utc: utc,
   });
+});
+
+app.get("/api/:s", function (req, res) {
+  input = req.params.s;
+  try {
+    const timestamp = Number(input);
+    if (!isNaN(timestamp)) {
+      date = new Date(timestamp);
+    } else {
+      date = new Date(input);
+    }
+    console.log(date);
+    const unix = date.getTime();
+    const utc = date.toUTCString();
+
+    if (date != "Invalid Date") {
+      res.json({
+        unix: unix,
+        utc: utc,
+      });
+    } else {
+      res.json({
+        error: "Invalid Date",
+      });
+    }
+  } catch (e) {
+    res.json({
+      error: e,
+    });
+  }
 });
 
 // Listen on port set in environment variable or default to 3000
