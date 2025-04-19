61e5960ab75bae43a7a87b3c4c9673c2887eb68b
Nick-legriffalo-Grif <56260217+legriffalo@users.noreply.github.com>
2025-04-19 18:31:03 +0300

    second api ready for validation added styling to demo pages

---
+++ API2-headers/server.js
@@ -0,0 +1,41 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require("express");
+const proxyAddr = require("proxy-addr");
+
+var app = express();
+app.set("trust proxy", true);
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/api/whoami", function (req, res) {
+  console.log("api called");
+
+  const ipaddress = req.ip;
+  const language = req.headers["accept-language"];
+  const software = req.headers["user-agent"];
+
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software,
+  });
+});
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
