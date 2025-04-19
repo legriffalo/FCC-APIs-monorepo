a530d5d451600f80ee036bea36985c4d228fded3
Nick-legriffalo-Grif <56260217+legriffalo@users.noreply.github.com>
2025-04-19 18:59:11 +0300

    added fetch and loading for cold starts

--- API1-time/server.js
+++ API1-time/server.js
@@ -64,6 +64,6 @@ app.get("/api/:s", function (req, res) {
 });
 
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3001, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
